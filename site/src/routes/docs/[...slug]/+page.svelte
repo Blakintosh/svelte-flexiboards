e9deec98c964687998f2858e7ff88c7d3497f459
Blakintosh <6821024+Blakintosh@users.noreply.github.com>
2025-04-10 09:12:43 +0100

    minor tweaks to docs

--- site/src/routes/docs/[...slug]/+page.svelte
+++ site/src/routes/docs/[...slug]/+page.svelte
@@ -13,28 +13,26 @@
 	});
 </script>
 
-<div class="flex h-full gap-16 relative">
+<div class="flex h-full gap-16 relative px-8">
 	<nav class="hidden shrink-0 w-64 lg:block">
 		<div class="sticky top-14">
-			<div class="flex flex-col h-[calc(100%-6.5rem)] max-h-[calc(100%-6.5rem)] min-h-0 overflow-y-auto gap-8 text-base border-r px-4 border-dashed py-12">
+			<div class="flex flex-col h-[calc(100%-6.5rem)] max-h-[calc(100%-6.5rem)] min-h-0 overflow-y-auto gap-8 text-base border-r pr-4 border-dashed py-12">
 				<DocumentationSidebar />
 			</div>
 		</div>
 	</nav>
-	<div class="grow flex items-start gap-16 overflow-y-auto px-8">
-		<article
-			class="prose prose-sm mx-auto w-full shrink-0 py-8 dark:prose-invert lg:prose-lg 2xl:prose-xl lg:py-12 overflow-y-auto"
-			id="docs-content"
-		>
-			<div class="not-prose">
-				<h1 class="mb-2 text-3xl font-bold text-foreground lg:mb-4 lg:text-4xl 2xl:text-5xl">
-					{data.doc.meta.title}
-				</h1>
-				<p class="text-muted-foreground">{data.doc.meta.description}</p>
-			</div>
-			<PageComponent />
-		</article>
-	</div>
+	<article
+		class="prose prose-sm mx-auto w-full shrink-0 py-8 dark:prose-invert lg:prose-lg 2xl:prose-xl lg:py-12 overflow-y-auto"
+		id="docs-content"
+	>
+		<div class="not-prose">
+			<h1 class="mb-2 text-3xl font-bold text-foreground lg:mb-4 lg:text-4xl 2xl:text-5xl">
+				{data.doc.meta.title}
+			</h1>
+			<p class="text-muted-foreground">{data.doc.meta.description}</p>
+		</div>
+		<PageComponent />
+	</article>
 	<aside class="hidden w-64 lg:block">
 		<div class="flex flex-col gap-8 text-base sticky top-14 py-12">
 			<Toc />
